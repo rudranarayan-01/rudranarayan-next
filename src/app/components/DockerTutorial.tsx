00d80ea884262d2a4e8d19dfbda8654d7d06a1e0
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-07-08 18:50:43 +0530

    fix: Remove unused Image import from DockerTutorial component

--- src/app/components/DockerTutorial.tsx
+++ src/app/components/DockerTutorial.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import CodeBlock from './CodeBlock';
 import TutorialLayout from './TutorialLayout';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 
 export default function DockerTutorial() {
   return (
