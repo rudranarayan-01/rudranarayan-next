c5d6f7a05254651826328b067a0cf6eded8e0915
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-07-09 13:57:17 +0530

    docker

--- src/app/components/DockerTutorial.tsx
+++ src/app/components/DockerTutorial.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import CodeBlock from './CodeBlock';
 import TutorialLayout from './TutorialLayout';
 import { motion } from 'framer-motion';
@@ -18,10 +19,42 @@ export default function DockerTutorial() {
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ delay: 0.2 }}
                 >
-                    <h1 className="text-4xl font-bold text-white mb-2">🚀 Docker Tutorial</h1>
+                    <h1 className="text-4xl font-bold text-white mb-2 justify-center">🚀 Docker Tutorial</h1>
+                    <li className="text-gray-300 text-lg">
+                        Docker is a open source platform
+                    </li>
+                    <li className="text-gray-300 text-lg">
+                        Using for building, running, managing applications by packing them into a standardize unit called container.
+                    </li>
+                    <li className="text-gray-300 text-lg">
+                        Docker has two components docker container and docker images. 
+                    </li>
+                </motion.div>
+
+                <motion.div
+                    initial={{ opacity: 0, x: 20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.3 }}
+                    className="space-y-4"
+                >
+                    <h2 className="text-2xl font-semibold text-white border-b border-gray-700 pb-1">Docker Container</h2>
                     <p className="text-gray-300 text-lg">
-                        Docker is a platform for packaging apps into containers.
+                        A standardized, executable unit of software that packages up code and all its dependencies, so the application runs quickly and reliably from one computing environment to another.
                     </p>
+                </motion.div>
+
+                <motion.div
+                    initial={{ opacity: 0, x: 20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.3 }}
+                    className="space-y-4"
+                >
+                    <h2 className="text-2xl font-semibold text-white border-b border-gray-700 pb-1">Docker Images</h2>
+                    <li className="text-gray-300 text-lg">
+                        A read-only template that contains the application code, runtime, system tools, libraries, and dependencies needed to run an application.
+                    </li>
+                    <li className='text-gray-300 text-lg'>A read-only template that contains the application code, runtime, system tools, libraries, and dependencies needed to run an application.
+                    </li>
                 </motion.div>
 
                 <motion.div
@@ -44,8 +77,34 @@ docker -v`} />
                     transition={{ delay: 0.4 }}
                     className="space-y-4"
                 >
-                    <h2 className="text-2xl font-semibold text-white border-b border-gray-700 pb-1">🎉 Run Hello World</h2>
+                    <h2 className="text-2xl font-semibold text-white border-b border-gray-700 pb-1">Docker Hub</h2>
+                    <p className='text-lg text-gray-300'>Docker hub contains all the public collections of docker images <Link className='text-blue-500' target='blank' href="https://hub.docker.com/">https://hub.docker.com/</Link></p>
+                </motion.div>
+
+                <motion.div
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.4 }}
+                    className="space-y-4"
+                >
+                    <h2 className="text-2xl font-semibold text-white border-b border-gray-700 pb-1">Docker Commands</h2>
+
+                    <li className='text-gray-300 text-lg'>docker pull IMAGE_NAME It is used to pull image from docker hub</li>
+                    <CodeBlock code={`docker pull hello-world`} />
+
+                    <li className='text-gray-300 text-lg'>docker images It shows all the available images</li>
+                    <CodeBlock code={`docker images`} />
+
+                    <li className='text-gray-300 text-lg'>docker run IMAGE_NAME Used to buiild container from image</li>
                     <CodeBlock code={`docker run hello-world`} />
+
+                    <li className='text-gray-300 text-lg'>docker run -it IMAGE_NAME Used to buiild container from image which will run in interractive mode</li>
+                    <CodeBlock code={`    docker pull ubuntu
+    docker run -it ubuntu`} />
+                    <li className='text-gray-300 text-lg'>Now we are inside ubuntu container</li>
+                    <CodeBlock code={`    docker pull ubuntu
+    docker run -it ubuntu`} />
+
                 </motion.div>
             </motion.div>
         </TutorialLayout>
